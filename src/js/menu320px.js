77ab7ba0c424a2483bcf7fcd5013afe5b5663658
SoundTroniX <maks.yangirov@mail.ru>
2025-02-15 10:57:41 +0500

    fix: overlay

--- src/js/menu320px.js
+++ src/js/menu320px.js
@@ -1,14 +1,30 @@
 let menuPage320 = document.querySelector('.menu');
 let burgerButtonMenu = document.querySelector('.menu__header_burger');
 let burgerButtonHeader = document.querySelector('.header__burger');
+let overlayPage = document.querySelector('.page__overlay');
 
 burgerButtonMenu.addEventListener('click', function () {
     // menuPage320.classList.remove('menu');
     menuPage320.classList.add('menu__close');
+    // overlay
+    overlayPage.classList.remove('page__overlay_visible');
+    overlayPage.classList.add('page__overlay');
 });
 
 burgerButtonHeader.addEventListener('click', function () {
     menuPage320.classList.remove('menu__close');
     menuPage320.classList.add('menu');
+    // overlay
+    overlayPage.classList.remove('page__overlay');
+    overlayPage.classList.add('page__overlay_visible');
 });
+
+document.addEventListener("click", function(e) {
+    if (e.target.className == "page__overlay_visible") {
+     menuPage320.classList.add('menu__close');
+    //  overlay
+    overlayPage.classList.remove('page__overlay_visible');
+    overlayPage.classList.add('page__overlay');
+    }
+  });
 
--- src/js/menu768px.js
+++ src/js/menu768px.js
@@ -1,13 +1,22 @@
 let menuPage = document.querySelector('.menu');
 let burgerButtonMenu = document.querySelector('.menu__header_burger');
 let burgerButtonHeader = document.querySelector('.header__burger');
+let overlayPage = document.querySelector('.page__overlay');
 
 burgerButtonMenu.addEventListener('click', function () {
     // menuPage.classList.remove('menu');
     menuPage.classList.add('menu__close');
+    // overlay
+    overlayPage.classList.remove('page__overlay_visible');
+    overlayPage.classList.add('page__overlay');
+    
 });
 
 burgerButtonHeader.addEventListener('click', function () {
     menuPage.classList.remove('menu__close');
     menuPage.classList.add('menu');
+    // overlay
+    overlayPage.classList.remove('page__overlay');
+    overlayPage.classList.add('page__overlay_visible');
+    
 });
